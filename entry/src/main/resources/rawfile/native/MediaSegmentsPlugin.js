42a45c574ac13727b2bef42597c401c368bce00f
JerryWang-xmu <1071641947@qq.com>
2026-03-22 18:37:44 +0800

    docs: add copyright headers to all source files
    Add standardized copyright headers to all TypeScript and JavaScript files to comply with licensing requirements. The headers include attribution to original Jellyfin contributors and note the rewrite in ArkTS by WangRuijie on 2026-03-22.

--- entry/src/main/resources/rawfile/native/MediaSegmentsPlugin.js
+++ entry/src/main/resources/rawfile/native/MediaSegmentsPlugin.js
@@ -1,3 +1,28 @@
+/*
+ * Copyright (C) 2020-2026 Jellyfin Contributors
+ * Copyright (C) 2026 WangRuijie
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * ---
+ *
+ * Based on jellyfin-android by Jellyfin Contributors
+ * Original repository: https://github.com/jellyfin/jellyfin-android
+ * Rewritten in 'ArkTS' with modifications by WangRuijie on 2026-03-22
+ */
+
 /**
  * Jellyfin HarmonyOS - Media Segments Plugin
  * 媒体段插件，用于跳过片头片尾等
